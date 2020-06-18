405fcb86c291284684ff01a37d3b6c38ae0d1c9f
Christopher Backhouse <bckhouse@fnal.gov>
2020-06-18 11:17:13 -0500

    Fix a couple of problems with OscCalcDumb that didn't show up before since we don't build it.

--- OscCalcDumb.h
+++ OscCalcDumb.h
@@ -5,7 +5,6 @@
 //////////////////////////////////////////////////////////
 
 #include "OscLib/IOscCalc.h"
-#include "Utilities/MathUtil.h"
 
 #include <cmath>
 
@@ -18,7 +17,8 @@ namespace osc
   {
   public:
     virtual IOscCalc* Copy() const {return new OscCalcDumb;}
-    virtual double P(int flavBefore, int flavAfter, double E)
+    using IOscCalc::P;
+    virtual double P(int flavBefore, int flavAfter, double E) override
     {
       const double L      = 810.;    // km
       const double ldm    = 2.40e-3; // large delta m^2, m23
@@ -29,37 +29,40 @@ namespace osc
       // Signal
       if(abs(flavAfter) == 12 && abs(flavBefore) == 14){
 	// Nue appearance
-	return ss2t13*ssth23*util::sqr(sin(1.267*ldm*L/E));
+	return ss2t13*ssth23*sqr(sin(1.267*ldm*L/E));
       }
       if(abs(flavAfter) == 14 && abs(flavBefore) == 14){
 	// CC mu disappearance
-	return 1-ss2t23*util::sqr(sin(1.267*ldm*L/E));
+	return 1-ss2t23*sqr(sin(1.267*ldm*L/E));
       }
 
       // Background
       if(abs(flavAfter) == 12 && abs(flavBefore) == 12){
 	// Beam nue
-	return 1-ss2t13*util::sqr(sin(1.267*ldm*L/E));
+	return 1-ss2t13*sqr(sin(1.267*ldm*L/E));
       }
       if(abs(flavAfter) == 14 && abs(flavBefore) == 12){
 	// CC mu appearance
-	return ss2t13*ssth23*util::sqr(sin(1.267*ldm*L/E));
+	return ss2t13*ssth23*sqr(sin(1.267*ldm*L/E));
       }
       if(abs(flavAfter) == 16 && abs(flavBefore) == 14){
         //numu to nutau CC appearance
-        return (1-ss2t13)*ss2t23*util::sqr(sin(1.267*ldm*L/E));
+        return (1-ss2t13)*ss2t23*sqr(sin(1.267*ldm*L/E));
       }
       if(abs(flavAfter) == 16 && abs(flavBefore) == 12){
         //nue to nutau CC appearance
-        return ss2t13*(1-ssth23)*util::sqr(sin(1.267*ldm*L/E));
+        return ss2t13*(1-ssth23)*sqr(sin(1.267*ldm*L/E));
       }
       if(abs(flavAfter) == 16 && abs(flavBefore) == 16){
         //nutau to nutau CC disappearance
-        return 1-(ss2t23-ss2t23*ss2t13+ss2t13-ss2t13*ssth23)*util::sqr(sin(1.267*ldm*L/E));
+        return 1-(ss2t23-ss2t23*ss2t13+ss2t13-ss2t13*ssth23)*sqr(sin(1.267*ldm*L/E));
       }
 
       // Don't know what this is
       return 0;
     }
+
+  protected:
+    template<class T> T sqr(T x) const{return x*x;}
   };
 }
