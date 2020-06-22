55fd8a9609a4f20ea05dce1ceee11aefb6eee106
Christopher Backhouse <bckhouse@fnal.gov>
2020-06-22 16:25:38 -0500

    Start of jenkins scripting.

---
+++ jenkins/jenkins_collect.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TAG=v00.01
+mkdir $TAG
+
+ls
+
+for oldname in 'os='*
+do
+    newname=oldname
+    newname=${newname/os=SLF6/slf6.x86_64}
+    newname=${newname/qualifier=/}
+    newname=${newname/stan=/}
+    newname=${newname/,/.}
+    newname=${newname/;/.}
+    mv $oldname $TAG/$newname
+    mkdir $TAG/$newname/bin
+    mkdir $TAG/$newname/lib
+done
+
+cd $TAG
+mkdir src
+mkdir include
+mkdir ups
+touch ups/osclib.table
