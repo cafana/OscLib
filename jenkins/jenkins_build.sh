1d766e82b295b67dc5c6036cd2d99f6bf9df6749
Christopher Backhouse <bckhouse@fnal.gov>
2020-08-11 09:42:38 -0500

    Adding e19 support

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -4,9 +4,19 @@ set +ex
 env
 
 source /cvmfs/nova.opensciencegrid.org/externals/setup || exit 1
-setup root v6_16_00 -q $QUALIFIER || exit 1
+
+if [[ $QUALIFIER == *e19* ]]
+then
+    # DUNE lblpwgtools versions
+    setup root v6_18_04d -q $QUALIFIER || exit 1
+    setup boost v1_70_0 -q $QUALIFIER || exit 1
+else
+    # NOvA versions
+    setup root v6_16_00 -q $QUALIFIER || exit 1
+    setup boost v1_66_0a -q $QUALIFIER || exit 1
+fi
+
 setup eigen v3.3.5 || exit 1
-setup boost v1_66_0a -q $QUALIFIER || exit 1
 
 if [ $STAN == stan ]
 then
