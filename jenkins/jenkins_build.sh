d485ef4d368e4493fa8a19910727aa4f90bb6510
Christopher Backhouse <bckhouse@fnal.gov>
2020-08-11 09:51:04 -0500

    stan_math has inconsistenly-labelled versions

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -3,15 +3,15 @@
 set +ex
 env
 
-source /cvmfs/nova.opensciencegrid.org/externals/setup || exit 1
-
 if [[ $QUALIFIER == *e19* ]]
 then
     # DUNE lblpwgtools versions
+    source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || exit 1
     setup root v6_18_04d -q ${QUALIFIER}:py2 || exit 1
     setup boost v1_70_0 -q $QUALIFIER || exit 1
 else
     # NOvA versions
+    source /cvmfs/nova.opensciencegrid.org/externals/setup || exit 1
     setup root v6_16_00 -q $QUALIFIER || exit 1
     setup boost v1_66_0a -q $QUALIFIER || exit 1
 fi
