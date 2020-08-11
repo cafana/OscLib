67b17d3c4765f1661d59ed47e3691d513c9f7142
Christopher Backhouse <bckhouse@fnal.gov>
2020-08-11 09:54:00 -0500

    Same problem with eigen.

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -9,24 +9,15 @@ then
     source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || exit 1
     setup root v6_18_04d -q ${QUALIFIER}:py2 || exit 1
     setup boost v1_70_0 -q $QUALIFIER || exit 1
+    setup eigen v3_3_5 || exit 1
+    if [ $STAN == stan ]; then setup stan_math v2_18_0 -q $QUALIFIER || exit 1; fi
 else
     # NOvA versions
     source /cvmfs/nova.opensciencegrid.org/externals/setup || exit 1
     setup root v6_16_00 -q $QUALIFIER || exit 1
     setup boost v1_66_0a -q $QUALIFIER || exit 1
-fi
-
-setup eigen v3.3.5 || exit 1
-
-if [ $STAN == stan ]
-then
-    if [[ $QUALIFIER == *e19* ]]
-    then
-        # This is silly...
-        setup stan_math v2_18_0 -q $QUALIFIER || exit 1
-    else
-        setup stan_math v2.18.0 -q $QUALIFIER || exit 1
-    fi
+    setup eigen v3.3.5 || exit 1
+    if [ $STAN == stan ]; then setup stan_math v2.18.0 -q $QUALIFIER || exit 1; fi
 fi
 
 make clean # don't trust my build system
