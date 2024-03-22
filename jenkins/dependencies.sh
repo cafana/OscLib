d60b7e502dd3b63198395836bc3beaae0cbd6495
Gavin Davies <gsdavies@fnal.gov>
2024-03-21 23:13:41 -0500

    prepping for latest art upgrade, upon request from SBN - addressing #19 - nulite v3_16_04a carries latest art 3.14.04 upgrade - thus new qualifier n316 - new root and boost, with updated p3915 python qualifier - e26 and c14 exist for this upgrade atm

--- jenkins/dependencies.sh
+++ jenkins/dependencies.sh
@@ -12,6 +12,7 @@ QUAL=$1
 
 if [[ $QUAL == *:n313* ]]; then NQUAL=n313; QUAL=${QUAL/:n313/}; fi
 if [[ $QUAL == *:n315* ]]; then NQUAL=n315; QUAL=${QUAL/:n315/}; fi
+if [[ $QUAL == *:n316* ]]; then NQUAL=n316; QUAL=${QUAL/:n316/}; fi
 
 WANTSTAN=yes
 if [[ $QUAL == *:stanfree ]]; then WANTSTAN=no; QUAL=${QUAL/:stanfree/}; else QUAL=${QUAL/:stan/}; fi
@@ -21,10 +22,15 @@ then
     # These are the current (May 2023) nova versions (nutools v3_13_04b)
     echo root v6_22_08d -q${QUAL}:p392
     echo boost v1_75_0 -q$QUAL
-else
+elif [[ $NQUAL == n315 ]]
+then
     # These are the current (July 2023) sbn versions (nutools v3_15_03)
     echo root v6_26_06b -q${QUAL}:p3913
     echo boost v1_80_0 -q$QUAL
+else
+    # nulite v3_16_04a
+    echo root v6_28_10b -q${QUAL}:p3915
+    echo boost v1_82_0 -q$QUAL
 fi
 
 echo eigen v3_4_0
