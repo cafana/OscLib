a47a0637fe2f550901295f9472f13d048d1ccfa9
Cullen Sullivan <cullenmsullivan@gmail.com>
2024-08-14 11:28:21 -0500

    Integrating OscCalcNuFast with Stan, mimicking the caching mechanism OscCalcAnalytic uses

--- OscLib/OscCalcNuFast.cxx
+++ OscLib/OscCalcNuFast.cxx
@@ -9,16 +9,18 @@
 #include <cmath>
 #include <iostream>
 #include <limits>
+#include <tuple>
+#include <vector>
 
 namespace osc {
   template<typename T>
   _OscCalcNuFast<T>::_OscCalcNuFast(void) :
-    fYe(0.5), fNNewton(1), fDirty(true), fCurrEnergy(0)
+    fYe(0.5), fNNewton(1), fIsAggressive(true), fIsDirty(true)
     {}
   
   template<typename T>
   _OscCalcNuFast<T>::_OscCalcNuFast(const _OscCalcNuFast<T>& other) :
-    _IOscCalcAdjustable<T>(other), fYe(other.GetYe()), fNNewton(other.GetNNewton()), fDirty(true), fCurrEnergy(0)
+    _IOscCalcAdjustable<T>(other), fYe(other.GetYe()), fNNewton(other.GetNNewton()), fIsAggressive(true), fIsDirty(true)
     {}
   
   template<typename T>
@@ -29,29 +31,32 @@ namespace osc {
   
   template<typename T>
   void _OscCalcNuFast<T>::SetTh12(const T& th12) {
-    this->fDirty = true;
+    this->fIsDirty = true;
     this->fTh12 = th12;
     // Precomute sine squared of th12.
-    T s12 = sin(th12); this->fPre.s12sq = s12*s12; // Fast square that doesn't need to compute sine twice.
+    this->fPre.s12 = sin(th12);
+    this->fPre.s12sq = this->fPre.s12*this->fPre.s12;
   }
   
   template<typename T>
   void _OscCalcNuFast<T>::SetTh13(const T& th13) {
-    this->fDirty = true;
+    this->fIsDirty = true;
     this->fTh13 = th13;
-    T s13 = sin(th13); this->fPre.s13sq = s13*s13;
+    this->fPre.s13 = sin(th13);
+    this->fPre.s13sq = this->fPre.s13*this->fPre.s13;
   }
   
   template<typename T>
   void _OscCalcNuFast<T>::SetTh23(const T& th23) {
-    this->fDirty = true;
+    this->fIsDirty = true;
     this->fTh23 = th23;
-    T s23 = sin(th23); this->fPre.s23sq = s23*s23;
+    this->fPre.s23 = sin(th23);
+    this->fPre.s23sq = this->fPre.s23*this->fPre.s23;
   }
   
   template<typename T>
   void _OscCalcNuFast<T>::SetdCP(const T& dCP) {
-    this->fDirty = true;
+    this->fIsDirty = true;
     this->fdCP = dCP;
     // Precompute the sine and cosine of delta CP.
     this->fPre.sind = sin(dCP);
@@ -84,34 +89,41 @@ namespace osc {
   //                            double L, double E, double rho,
   //                            double Ye, int N_Newton, double (*probs_returned)[3][3])
   //{
-  // We instead use a function signature consistent with OscLib conventions.
-  template<typename T>
-  T _OscCalcNuFast<T>::P(int flavBefore, int flavAfter, double E) {
+  template<typename T> template<class VT, class KVT>
+  VT _OscCalcNuFast<T>::_P(int from, int to, KVT E) {
     // First, make sure the PDG codes have the same sign.
-    static bool hasWarnedAboutSignChange = false;
-    if ( (flavBefore > 0 && flavAfter < 0) || (flavBefore < 0 && flavAfter > 0) ) {
-      // They're asking to change neutrino sign? Warn and return no probability.
-      if ( !hasWarnedAboutSignChange ) {
-        std::cout << "NuFast warning: Requesting probability with change in sign (" << flavBefore << " -> " << flavAfter << ")" << std::endl;
-        hasWarnedAboutSignChange = true;
-      }
-      return 0;
-    }
+    assert((from > 0 && to > 0) || (from < 0 && to < 0) );
+    // Nice trick from analytic: If we need antineutrinos, just flip flavor signs and send E -> -E. The NuFast algorithm already uses -E means antineutrinos.
+    if ( from < 0 ) { return P(-from,-to,-E); }
     
     // Recompute energy independent PMNS terms if we've updated any parameters.
-    if ( this->fDirty ) {
+    if ( !this->fIsAggressive || this->fIsDirty ) {
+      //std::cout << "NuFast: Recomputing energy independent terms." << std::endl;
       this->RecomputeEnergyIndependentTerms();
     }
     
-    bool isAnti = flavBefore < 0;
+    // Try to find the requested energy in the probabilities cache. E is possibly negative (for antineutrinos).
+    typename analytic::ProbCache<KVT,VT>::iterator it = analytic::ProbCache<KVT,VT>::find(E);
+    if ( it != analytic::ProbCache<KVT,VT>::end() ) {
+      //std::cout << "NuFast: Just using probability cache." << std::endl;
+      return it->second.P(from,to);
+    }
+    
+    // Need to do energy-dependent computation.
+    //std::cout << "NuFast: Recomputing probability matrix and returning result." << std::endl;
+    return this->RecomputeProbabilityMatrix<VT,KVT>(from,to,E);
+    
     // Recompute the probability matrix if necessary (either a parameter has been updated or we used a different energy before).
     //std::cout << "E = " << E << ", currE = " << this->fCurrEnergy << ", diff = " << this->fCurrEnergy - (isAnti ? -E : E) << std::endl;
+    /*
     if ( std::abs(this->fCurrEnergy - (isAnti ? -E : E)) > std::numeric_limits<double>::epsilon() ) {
     //  std::cout << "Nufast info: Recomputing probability matrix..." << std::endl;
       this->RecomputeProbabilityMatrix(E,isAnti);
     }
+    */
     
     // We have good PDG codes. Now return the corresponding element of the probability matrix.
+    /*
     unsigned int indexBefore = -1, indexAfter = -1;
     // I'd use std::unordered_map for pdg->index, but this should be faster.
     if ( flavBefore == +12 || flavBefore == -12 ) { indexBefore = 0; }
@@ -121,7 +133,7 @@ namespace osc {
     if ( flavAfter  == +14 || flavAfter  == -14 ) { indexAfter  = 1; }
     if ( flavAfter  == +16 || flavAfter  == -16 ) { indexAfter  = 2; }
     assert( indexBefore != -1 && indexAfter != -1 ); // This checks that we got PDG codes that are actually neutrinos.
-    
+    */
     // Probability matrix:
     //std::cout << "E = " << E << ", isAnti = " << isAnti << std::endl;
     //std::cout << this->fCurrProbs[0][0] << " " << this->fCurrProbs[0][1] << " " << this->fCurrProbs[0][2] << std::endl
@@ -129,40 +141,48 @@ namespace osc {
     //          << this->fCurrProbs[2][0] << " " << this->fCurrProbs[2][1] << " " << this->fCurrProbs[2][2] << std::endl;
     //std::cout << "Returning prob " << indexBefore << " -> " << indexAfter << ": " << this->fCurrProbs[indexBefore][indexAfter] << std::endl;
     
-    return this->fCurrProbs[indexBefore][indexAfter];
+    //return this->fCurrProbs[indexBefore][indexAfter];
+  }
+  
+  template<typename T> T _OscCalcNuFast<T>::P(int from, int to, double E) {
+    //std::cout << "NuFast: Calling double overload." << std::endl;
+    return _P<T>(from,to,E);
+  }
+  
+  template<typename T> Eigen::ArrayX<T> _OscCalcNuFast<T>::P(int from, int to, const Eigen::ArrayXd& E)
+  {
+    //std::cout << "NuFast: Calling Eigen::ArrayXd overload." << std::endl;
+    return _P<Eigen::ArrayX<T>>(from,to,E);
+  }
+  
+  template<typename T> Eigen::ArrayX<T> _OscCalcNuFast<T>::P(int from, int to, const std::vector<double>& E)
+  {
+    //std::cout << "NuFast: Calling std::vector<double> overload (size = " << E.size() << ")." << std::endl;
+    return P(from,to,Eigen::Map<const Eigen::ArrayXd>(E.data(),E.size()));
   }
   
   template<typename T>
   void _OscCalcNuFast<T>::RecomputeEnergyIndependentTerms(void) {
-    //#define Ut2sq this->fPre.s13sq * this->fPre.s12sq * this->fPre.s23sq
-    //#define Um2sq (1-this->fPre.s12sq) * (1-this->fPre.s23sq)
-    //this->fPre.Jrr = sqrt( Um2sq * Ut2sq );
-    //this->fPre.Jmatter = 8 * this->fPre.Jrr * (1-this->fPre.s13sq) * this->fPre.sind;
-    //#undef Ut2sq
-    //#undef Um2sq
+    // NOvA doesn't use dmsq31 by convention, but we can compute it if we know dmsq32 and dmsq21.
+    this->fPre.Dmsq31 = this->fDmsq32 + this->fDmsq21; // dmsq31 = msq3 - msq1 = (msq3-msq2) + (msq2-msq1) = dmsq32 + dmsq21.
+    this->fPre.Dmsqee = this->fPre.Dmsq31 - this->fPre.s12sq * this->fDmsq21;
     this->fPre.c13sqxs12sq = (1-this->fPre.s13sq) * this->fPre.s12sq;
     this->fPre.c13sqxs23sq = (1-this->fPre.s13sq) * this->fPre.s23sq;
     this->fPre.c12sqxc23sq = (1-this->fPre.s12sq) * (1-this->fPre.s23sq);
     this->fPre.s13sqxs12sqxs23sq = this->fPre.s13sq * this->fPre.s12sq * this->fPre.s23sq;
-    this->fDirty = false;
+    this->fPre.Jrr = sqrt(this->fPre.c12sqxc23sq * this->fPre.s13sqxs12sqxs23sq);
+    this->fPre.Jmatter_first = 8 * this->fPre.Jrr * (1-this->fPre.s13sq) * this->fPre.sind 
+                                 * this->fDmsq21 * this->fPre.Dmsq31 * (this->fPre.Dmsq31 - this->fDmsq21);
+    this->fPre.Um2sq_first = this->fPre.c12sqxc23sq + this->fPre.s13sqxs12sqxs23sq - 2 * this->fPre.Jrr * this->fPre.cosd;
+    this->fPre.See = this->fDmsq21+this->fPre.Dmsq31 - this->fDmsq21 * this->fPre.c13sqxs12sq - this->fPre.Dmsq31 * this->fPre.s13sq;
+    this->fPre.Tee = this->fDmsq21 * this->fPre.Dmsq31 * (1 - this->fPre.s13sq - this->fPre.c13sqxs12sq);
+    ClearProbCaches(); // The caches were invalidated if this step was necessary.
+    this->fIsDirty = false;
   }
   
-  template<typename T>
-  void _OscCalcNuFast<T>::RecomputeProbabilityMatrix(double E, bool isAnti) {
-    // Internally in NuFast, positive energy is for neutrinos and negative energy is for antineutrinos.
-    if ( E < 0 ) {
-      std::cerr << "NuFast error: RecomputeProbabilityMatrix should be passed E >= 0. Pass isAnti = true for antineutrinos." << std::endl;
-      abort();
-    }
-    if ( isAnti ) { E = -E; }
-    this->fCurrEnergy = E; // Save the energy to test if these results can be reused in the future.
-    
+  template<typename T> template<class VT, class KVT>
+  VT _OscCalcNuFast<T>::RecomputeProbabilityMatrix(int from, int to, KVT E) {
     // Now we need to interface internally used NuFast variables with the OscCalc members.
-    // The following variables and definitions are designed to efficiently interface with NuFast without rewriting its guts.
-    //T s12sq = sin(this->fTh12); s12sq = s12sq*s12sq; // Fast square that doesn't compute sine twice.
-    //T s13sq = sin(this->fTh13); s13sq = s13sq*s13sq;
-    //T s23sq = sin(this->fTh23); s23sq = s23sq*s23sq;
-    
     #define s12sq this->fPre.s12sq
     #define s13sq this->fPre.s13sq
     #define s23sq this->fPre.s23sq
@@ -174,8 +194,6 @@ namespace osc {
     
     #define delta this->fdCP
     #define Dmsq21 this->fDmsq21
-    // NOvA doesn't use dmsq31 by convention, but we can compute it if we know dmsq32 and dmsq21.
-    T Dmsq31 = this->fDmsq32 + this->fDmsq21; // dmsq31 = msq3 - msq1 = (msq3-msq2) + (msq2-msq1) = dmsq32 + dmsq21.
     #define L this->fL
     #define rho this->fRho
     #define Ye this->fYe
@@ -183,61 +201,49 @@ namespace osc {
     #define probs_returned this->fCurrProbs
     
     // Aggressive precomputation. For terms that don't depend on energy, refer to the cached precomputations.
+    #define Dmsq31 this->fPre.Dmsq31
+    #define Dmsqee this->fPre.Dmsqee
     #define c13sqxs12sq this->fPre.c13sqxs12sq
     #define c13sqxs23sq this->fPre.c13sqxs23sq
     #define c12sqxc23sq this->fPre.c12sqxc23sq
     #define s13sqxs12sqxs23sq this->fPre.s13sqxs12sqxs23sq
     #define Jrr this->fPre.Jrr
-    #define Jmatter this->fPre.Jmatter
+    #define Jmatter_first this->fPre.Jmatter_first
+    #define Um2sq_first this->fPre.Um2sq_first
+    #define See this->fPre.See
+    #define Tee this->fPre.Tee
     
-    // Now here begins the (almost exclusively) untouched NuFast algorithm.
-    T /*c13sq, sind, cosd, Jrr, Jmatter,*/ Dmsqee, Amatter;
-    T Ue1sq, Ue2sq, Ue3sq, Um1sq, Um2sq, Um3sq, Ut1sq, Ut2sq, Ut3sq;
-    T A, B, C;
-    T See, Tee, Smm, Tmm;
-    T xmat, lambda2, lambda3, Dlambda21, Dlambda31, Dlambda32;
-    T Xp2, Xp3, PiDlambdaInv, tmp;
-    T Lover4E, D21, D32;
-    T sinD21, sinD31, sinD32;
-    T sinsqD21_2, sinsqD31_2, sinsqD32_2, triple_sin;
-    T Pme_CPC, Pme_CPV, Pmm, Pee;
+//    VT /*c13sq, sind, cosd, Jrr,*/ Jmatter, /*Dmsqee,*/ Amatter;
+//    VT Ue1sq, Ue2sq, Ue3sq, Um1sq, Um2sq, Um3sq, Ut1sq, Ut2sq, Ut3sq;
+//    VT A, B, C;
+//    VT /*See, Tee,*/ Smm, Tmm;
+//   VT xmat, lambda2, lambda3, Dlambda21, Dlambda31, Dlambda32;
+//    VT Xp2, Xp3, PiDlambdaInv/*, tmp*/;
+//    VT Lover4E, D21, D32;
+//    VT sinD21, sinD31, sinD32;
+//    VT sinsqD21_2, sinsqD31_2, sinsqD32_2, triple_sin;
+//    VT Pme_CPC, Pme_CPV, Pmm, Pee;
     
     // --------------------------------------------------------------------- //
     // First calculate useful simple functions of the oscillation parameters //
     // --------------------------------------------------------------------- //
-    //c13sq = 1 - s13sq;
     
-    //T c13sqxs12sq = c13sq * s12sq;
-    //T c13sqxs23sq = c13sq * s23sq;
-    //T c12sqxc23sq = c12sq * c23sq;
-    //T s13sqxs12sqxs23sq = s13sq * s12sq * s23sq;
-    
-    Jrr = sqrt(c12sqxc23sq * s13sqxs12sqxs23sq);
-    
-    Um2sq = c12sqxc23sq + s13sqxs12sqxs23sq - 2 * Jrr * cosd;
-    Jmatter = 8 * Jrr * c13sq * sind;
-    Amatter = Ye * rho * E * YerhoE2a;
-    Dmsqee = Dmsq31 - s12sq * Dmsq21;
+    const VT Amatter = (Ye*rho*YerhoE2a) * E;
     
     // calculate A, B, C, See, Tee, and part of Tmm
-    A = Dmsq21 + Dmsq31; // temporary variable
-    See = A - Dmsq21 * c13sqxs12sq - Dmsq31 * s13sq;
-    Tmm = Dmsq21 * Dmsq31; // using Tmm as a temporary variable	  
-    Tee = Tmm * (1 - s13sq - c13sqxs12sq);
-    C = Amatter * Tee;
-    A = A + Amatter;
+    const VT C = Amatter * Tee;
+    const VT A = Dmsq21+Dmsq31 + Amatter;
     
     // ---------------------------------- //
     // Get lambda3 from lambda+ of MP/DMP //
     // ---------------------------------- //
-    xmat = Amatter / Dmsqee;
-    tmp = 1 - xmat;
-    lambda3 = Dmsq31 + 0.5 * Dmsqee * (xmat - 1 + sqrt(tmp * tmp + 4 * s13sq * xmat));
+    const VT xmat = Amatter / Dmsqee;
+    VT lambda3 = Dmsq31 + (0.5 * Dmsqee) * (xmat - 1 + sqrt((1-xmat) * (1-xmat) + (4*s13sq) * xmat));
     
     // ---------------------------------------------------------------------------- //
     // Newton iterations to improve lambda3 arbitrarily, if needed, (B needed here) //
     // ---------------------------------------------------------------------------- //
-    B = Tmm + Amatter * See; // B is only needed for N_Newton >= 1
+    const VT B = Dmsq21*Dmsq31 + Amatter * See; // B is only needed for N_Newton >= 1
     for (int i = 0; i < N_Newton; i++) {
       lambda3 = (lambda3 * lambda3 * (lambda3 + lambda3 - A) + C) / (lambda3 * (2 * (lambda3 - A) + lambda3) + B); // this strange form prefers additions to multiplications
     }
@@ -245,82 +251,82 @@ namespace osc {
     // ------------------- //
     // Get  Delta lambda's //
     // ------------------- //
-    tmp = A - lambda3;
-    Dlambda21 = sqrt(tmp * tmp - 4 * C / lambda3);
-    lambda2 = 0.5 * (A - lambda3 + Dlambda21);
-    Dlambda32 = lambda3 - lambda2;
-    Dlambda31 = Dlambda32 + Dlambda21;
+    const VT Dlambda21 = sqrt((A - lambda3) * (A - lambda3) - 4 * C / lambda3);
+    const VT lambda2 = 0.5 * (A - lambda3 + Dlambda21);
+    const VT Dlambda32 = lambda3 - lambda2;
+    const VT Dlambda31 = Dlambda32 + Dlambda21;
     
     // ----------------------- //
     // Use Rosetta for Veisq's //
     // ----------------------- //
     // denominators	  
-    PiDlambdaInv = 1 / (Dlambda31 * Dlambda32 * Dlambda21);
-    Xp3 = PiDlambdaInv * Dlambda21;
-    Xp2 = -PiDlambdaInv * Dlambda31;
+    const VT PiDlambdaInv = 1 / (Dlambda31 * Dlambda32 * Dlambda21);
+    const VT Xp3 = PiDlambdaInv * Dlambda21;
+    const VT Xp2 = -PiDlambdaInv * Dlambda31;
     
     // numerators
-    Ue3sq = (lambda3 * (lambda3 - See) + Tee) * Xp3;
-    Ue2sq = (lambda2 * (lambda2 - See) + Tee) * Xp2;
+    const VT Ue3sq = (lambda3 * (lambda3 - See) + Tee) * Xp3;
+    const VT Ue2sq = (lambda2 * (lambda2 - See) + Tee) * Xp2;
     
-    Smm = A - Dmsq21 * Um2sq - Dmsq31 * c13sqxs23sq;
-    Tmm = Tmm * (1 - c13sqxs23sq - Um2sq) + Amatter * (See + Smm - A);
+    const VT Smm = A - Dmsq21 * Um2sq_first - Dmsq31 * c13sqxs23sq;
+    const VT Tmm = Dmsq21*Dmsq31 * (1 - c13sqxs23sq - Um2sq_first) + Amatter * (See + Smm - A);
     
-    Um3sq = (lambda3 * (lambda3 - Smm) + Tmm) * Xp3;
-    Um2sq = (lambda2 * (lambda2 - Smm) + Tmm) * Xp2;
+    const VT Um3sq = (lambda3 * (lambda3 - Smm) + Tmm) * Xp3;
+    const VT Um2sq = (lambda2 * (lambda2 - Smm) + Tmm) * Xp2;
     
     // ------------- //
     // Use NHS for J //
     // ------------- //
-    Jmatter = Jmatter * Dmsq21 * Dmsq31 * (Dmsq31 - Dmsq21) * PiDlambdaInv;
+    const VT Jmatter = Jmatter_first * PiDlambdaInv;
     
     // ----------------------- //
     // Get all elements of Usq //
     // ----------------------- //
-    Ue1sq = 1 - Ue3sq - Ue2sq;
-    Um1sq = 1 - Um3sq - Um2sq;
+    const VT Ue1sq = 1 - Ue3sq - Ue2sq;
+    const VT Um1sq = 1 - Um3sq - Um2sq;
     
-    Ut3sq = 1 - Um3sq - Ue3sq;
-    Ut2sq = 1 - Um2sq - Ue2sq;
-    Ut1sq = 1 - Um1sq - Ue1sq;
+    const VT Ut3sq = 1 - Um3sq - Ue3sq;
+    const VT Ut2sq = 1 - Um2sq - Ue2sq;
+    const VT Ut1sq = 1 - Um1sq - Ue1sq;
     
     // ----------------------- //
     // Get the kinematic terms //
     // ----------------------- //
-    Lover4E = eVsqkm_to_GeV_over4 * L / E;
+    const VT Lover4E = (eVsqkm_to_GeV_over4 * L) / E;
     
-    D21 = Dlambda21 * Lover4E;
-    D32 = Dlambda32 * Lover4E;
+    const VT D21 = Dlambda21 * Lover4E;
+    const VT D32 = Dlambda32 * Lover4E;
 	  
-    sinD21 = sin(D21);
-    sinD31 = sin(D32 + D21);
-    sinD32 = sin(D32);
+    const VT sinD21 = sin(D21);
+    const VT sinD31 = sin(D32 + D21);
+    const VT sinD32 = sin(D32);
     
-    triple_sin = sinD21 * sinD31 * sinD32;
+    const VT triple_sin = sinD21 * sinD31 * sinD32;
     
-    sinsqD21_2 = 2 * sinD21 * sinD21;
-    sinsqD31_2 = 2 * sinD31 * sinD31;
-    sinsqD32_2 = 2 * sinD32 * sinD32;
+    const VT sinsqD21_2 = 2 * sinD21 * sinD21;
+    const VT sinsqD31_2 = 2 * sinD31 * sinD31;
+    const VT sinsqD32_2 = 2 * sinD32 * sinD32;
     
     // ------------------------------------------------------------------- //
     // Calculate the three necessary probabilities, separating CPC and CPV //
     // ------------------------------------------------------------------- //
-    Pme_CPC = (Ut3sq - Um2sq * Ue1sq - Um1sq * Ue2sq) * sinsqD21_2
-            + (Ut2sq - Um3sq * Ue1sq - Um1sq * Ue3sq) * sinsqD31_2
-	          + (Ut1sq - Um3sq * Ue2sq - Um2sq * Ue3sq) * sinsqD32_2;
-    Pme_CPV = -Jmatter * triple_sin;
+    const VT Pme_CPC = (Ut3sq - Um2sq * Ue1sq - Um1sq * Ue2sq) * sinsqD21_2
+                     + (Ut2sq - Um3sq * Ue1sq - Um1sq * Ue3sq) * sinsqD31_2
+	                   + (Ut1sq - Um3sq * Ue2sq - Um2sq * Ue3sq) * sinsqD32_2;
+    const VT Pme_CPV = -Jmatter * triple_sin;
     
-    Pmm = 1 - 2 * (Um2sq * Um1sq * sinsqD21_2
-        + Um3sq * Um1sq * sinsqD31_2
-        + Um3sq * Um2sq * sinsqD32_2);
+    const VT Pmm = 1 - 2 * (Um2sq * Um1sq * sinsqD21_2
+                 + Um3sq * Um1sq * sinsqD31_2
+                 + Um3sq * Um2sq * sinsqD32_2);
     
-    Pee = 1 - 2 * (Ue2sq * Ue1sq * sinsqD21_2
-        + Ue3sq * Ue1sq * sinsqD31_2
-        + Ue3sq * Ue2sq * sinsqD32_2);
+    const VT Pee = 1 - 2 * (Ue2sq * Ue1sq * sinsqD21_2
+                 + Ue3sq * Ue1sq * sinsqD31_2
+                 + Ue3sq * Ue2sq * sinsqD32_2);
     
     // ---------------------------- //
     // Assign all the probabilities //
     // ---------------------------- //
+    /*
     probs_returned[0][0] = Pee;                                             // Pee
     probs_returned[0][1] = Pme_CPC - Pme_CPV;                               // Pem
     probs_returned[0][2] = 1 - Pee - probs_returned[0][1];                  // Pet
@@ -332,6 +338,13 @@ namespace osc {
     probs_returned[2][0] = 1 - Pee - probs_returned[1][0];                  // Pte
     probs_returned[2][1] = 1 - probs_returned[0][1] - Pmm;                  // Ptm
     probs_returned[2][2] = 1 - probs_returned[0][2] - probs_returned[1][2]; // Ptt
+    */
+    
+    // Store the probabilities in the cache.
+    const analytic::Probs<VT> ps(Pee,Pme_CPC - Pme_CPV,Pme_CPC + Pme_CPV,Pmm);
+    analytic::ProbCache<KVT, VT>::emplace(E,ps);
+    // Return the probability.
+    return ps.P(from,to);
     
     // Undefine all the things.
     #undef s12sq
--- OscLib/OscCalcNuFast.h
+++ OscLib/OscCalcNuFast.h
@@ -1,19 +1,24 @@
 
-#pragma once
+#ifndef OSCLIBCALCNUFAST_H
+#define OSCLIBCALCNUFAST_H
 
 #include <string>
 
+#include "OscLib/Cache.h"
 #include "OscLib/IOscCalc.h"
 
 #include "TMD5.h"
 
-class TMD5;
+namespace Eigen {
+	template<class T> using ArrayX = Eigen::Array<T, Eigen::Dynamic, 1>;
+}
 
-namespace osc {
-  
+namespace osc {  
   /// This class is an OscLib adapter for the NuFast algorithm (arXiv:2405.02400v1).
   template<typename T>
-  class _OscCalcNuFast : public _IOscCalcAdjustable<T> {
+  class _OscCalcNuFast : public _IOscCalcAdjustable<T>,
+                         protected analytic::ProbCache<double, T>,
+                         protected analytic::ProbCache<Eigen::ArrayXd, Eigen::ArrayX<T>> {
   public:
     _OscCalcNuFast(void);
     virtual ~_OscCalcNuFast(void);
@@ -21,25 +26,28 @@ namespace osc {
     virtual _OscCalcNuFast<T>* Copy(void) const override;
     
     // Define setter functions for oscillation parameters.
-    virtual void SetL     (double L       ) override { this->fDirty = true; this->fL = L; }
-    virtual void SetRho   (double rho     ) override { this->fDirty = true; this->fRho = rho; }
-    virtual void SetDmsq21(const T& dmsq21) override { this->fDirty = true; this->fDmsq21 = dmsq21; }
-    virtual void SetDmsq32(const T& dmsq32) override { this->fDirty = true; this->fDmsq32 = dmsq32; }
+    virtual void SetL     (double L       ) override { this->fIsDirty = true; this->fL = L; }
+    virtual void SetRho   (double rho     ) override { this->fIsDirty = true; this->fRho = rho; }
+    virtual void SetDmsq21(const T& dmsq21) override { this->fIsDirty = true; this->fDmsq21 = dmsq21; }
+    virtual void SetDmsq32(const T& dmsq32) override { this->fIsDirty = true; this->fDmsq32 = dmsq32; }
     virtual void SetTh12  (const T& th12  ) override;
     virtual void SetTh13  (const T& th13  ) override;
     virtual void SetTh23  (const T& th23  ) override;
     virtual void SetdCP   (const T& dCP   ) override;
     /// Electron density in matter used by NuFast (default = 0.5).
-    virtual void SetYe    (double Ye      ) { this->fDirty = true; this->fYe = Ye; }
+    virtual void SetYe    (double Ye      ) { this->fIsDirty = true; this->fYe = Ye; }
     /// Number of Newton steps to improve eigen{value}{vector} convergence (default = 0 seems fine, use 1 or 2 if you need super accuracy).
-    virtual void SetNNewton(double nNewton) { this->fDirty = true; this->fNNewton = nNewton; }
+    virtual void SetNNewton(double nNewton) { this->fIsDirty = true; this->fNNewton = nNewton; }
+    virtual void SetAggressive(bool isAggressive) { this->fIsAggressive = isAggressive; }
     
     // We don't need new getter functions except for Ye and number of Newton corrections (specific to NuFast).
     virtual double GetYe(void) const { return this->fYe; }
     virtual double GetNNewton(void) const { return this->fNNewton; }
     
     /// Implement the NuFast algorithm.
-    virtual T P(int flavBefore, int flavAfter, double E) override;
+    virtual T P(int from, int to, double E) override;
+    virtual Eigen::ArrayX<T> P(int from, int to, const std::vector<double>& E) override;
+    virtual Eigen::ArrayX<T> P(int from, int to, const Eigen::ArrayXd& E) override;
     
     /// Specialized printer function.
     virtual void Print(const std::string& prefix = "") const override;
@@ -49,33 +57,42 @@ namespace osc {
       return _IOscCalcAdjustable<T>::GetParamsHashDefault("NuFast");
     }
   protected:
+    void ClearProbCaches(void) {
+      analytic::ProbCache<double, T>::clear();
+      analytic::ProbCache<Eigen::ArrayXd, Eigen::ArrayX<T>>::clear();
+    }
     /// Copy constructor.
     _OscCalcNuFast(const _OscCalcNuFast<T>& other);
     /// Probability computation helper function.
-    inline void RecomputeEnergyIndependentTerms(void);
-    inline void RecomputeProbabilityMatrix(double E, bool isAnti);
+    void inline __attribute__((always_inline)) RecomputeEnergyIndependentTerms(void);
+    /// from and to aren't necessary for computing the matrix but are used for the return (so the cache doesn't need to be searched for the result).
+    template<class VT, class KVT> inline __attribute__((always_inline)) VT RecomputeProbabilityMatrix(int from, int to, KVT E);
+    // Master probability function called with some combination of primitives, stan::math::vars, and Eigen objects.
+    template<class VT, class KVT> inline __attribute__((always_inline)) VT _P(int from, int to, KVT E);
     
     /// Electron density in matter. NuFast recommends 0.5, and this choice matches other calcs.
     double fYe;
     /// Number of Newton steps to use to improve eigen{value}{vector} slns. Increase me for more accuracy and worse runtime.
     double fNNewton;
-    /// Current probabilities. Ex fCurrProbs[1][0] is P(mu->e). Updated only when parameters are reset.
-    // TODO Am I also updated when a different energy is used?
-    mutable T fCurrProbs[3][3];
+    /// Should we aggressively precompute terms not dependent on energy?
+    bool fIsAggressive;
     /// Store some precomputations that don't depend on energy.
-    mutable struct {
+    struct {
+      T Dmsq31; // Squared mass splitting between m3 and m1.
+      T Dmsqee;
+      T s12, s13, s23; // Sines of mixing angles.
       T s12sq, s13sq, s23sq; // Sine squared of mixing angles. Immediately updated on reset of relevant angle.
       T sind, cosd; // Sine and cosine of delta CP. Immediately updated on reset of dCP.
       T c13sqxs12sq, c13sqxs23sq, c12sqxc23sq; // This is cos(th13)^2 * sin(th12)^2, etc.
 		  T s13sqxs12sqxs23sq;
-      T Jrr; // Used for Jarlskog invariant. Depends on all mixing angles.
-      T Jmatter; // Jarlskog invariant. Depends on all mixing angles and delta.
-      // What follows are some energy-independent terms of the PMNS matrix.
+      T Jrr; // Used for Jarlskog invariant.
+      T Jmatter_first; // Jarlskog times masses before an energy-dependent term is used.
+      T Um2sq_first;
+      T See, Tee;
     } fPre;
+    
     /// Do we need to recompute the nontrivial energy-independent terms?
-    mutable bool fDirty;
-    /// What was the energy that we just used to calculate the probability matrix?
-    mutable double fCurrEnergy;
+    mutable bool fIsDirty;
     
     // Constants for unit conversions, using Fermi constant for matter effect.
     double const eVsqkm_to_GeV_over4 = 1e-9 / 1.97327e-7 * 1e3 / 4;
@@ -84,4 +101,6 @@ namespace osc {
   
   typedef _OscCalcNuFast<double> OscCalcNuFast;
 }
+
+#endif
 
