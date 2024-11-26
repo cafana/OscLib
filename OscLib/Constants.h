3f5119cd05ff131a8c586e1ae697078378d098be
Cullen Sullivan <cullenmsullivan@gmail.com>
2024-11-26 13:52:57 -0600

    New file keeping physical constants and unit conversions in one place

---
+++ OscLib/Constants.h
@@ -0,0 +1,14 @@
+
+#ifndef OSCLIB_CONSTANTS
+#define OSCLIB_CONSTANTS
+
+// \file Constants.h
+// \brief Keep important physical constants and unit conversions in one place.
+//        All values use the natural units system such that hbar = c = 1.
+// \author <cullenms@fnal.gov>
+
+/// G_F, the Fermi Constant in GeV^-2. (PDG, Physical Constants, 2014).
+const double kFermiConstant = 1.1663787e-5;
+
+#endif // OSCLIB_CONSTANTS
+
