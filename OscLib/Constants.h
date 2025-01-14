c76697788eeee8f5c65f03d3f30241ea81f97cf8
Cullen Sullivan <cullenmsullivan@gmail.com>
2025-01-14 12:11:13 -0600

    Express hbar in GeV*s instead of J*s

--- OscLib/Constants.h
+++ OscLib/Constants.h
@@ -25,20 +25,17 @@ const double kFermiConstant = 1.1663788e-5;
 /// N_A, Avogadro's number in mol^-1 (or if you prefer, electrons/mol when thinking about the matter effect).
 const double kAvogadroConstant = 6.02214076e23;
 
-/// c, the speed of light (unitless). Multiply by c in natural units to convert s to m.
+/// c, the speed of light (m/s = unitless). Multiply by c in natural units to convert s to m.
 const double kSpeedOfLight = 299792458;
 
-/// hbar, reduced Planck's constant (unitless). Multiply by hbar in natural units to convert invese seconds to joules.
-const double kReducedPlanckConstant = 1.054571817e-34;
-
-/// e, the elementary charge (J/eV).
-const double kElementaryCharge = 1.602176364e-19;
+/// hbar, reduced Planck's constant (GeV*s = unitless). Multiply by hbar in natural units to convert 
+const double kReducedPlanckConstant = 6.582119569e-25;
 
 /// Unit conversion: m^-1 to eV.
 const double kInversemToeV =
     kSpeedOfLight          // 1 m^-1 = SpeedOfLight s^-1.
   * kReducedPlanckConstant // 1 s^-1 = ReducedPlanckConstant J.
-  / kElementaryCharge;     // ElementaryCharge J^-1 = eV.
+  / kGeVToeV;              // Finally convert GeV to eV.
 
 /// Given a matter density, rho in mol/cm^3, multiply by 2*sqrt(2)*FermiConstant and convert units to eV.
 const double kMatterDensityToEffect =
