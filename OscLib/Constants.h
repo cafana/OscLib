e336d7e1a275948640f31f94accb82f15bab7c67
Cullen Sullivan <cullenmsullivan@gmail.com>
2025-07-12 12:42:17 -0500

    Make constants constexpr inline

--- OscLib/Constants.h
+++ OscLib/Constants.h
@@ -14,37 +14,38 @@ namespace osc {
 namespace constants {
 
 /// Unit conversion: GeV to eV.
-const double kGeVToeV = 1e9;
+constexpr inline double kGeVToeV = 1e9;
 
 /// Unit conversion: km to m.
-const double kkmTom = 1e3;
+constexpr inline double kkmTom = 1e3;
 
 /// G_F, the Fermi Constant in GeV^-2.
-const double kFermiConstant = 1.1663788e-5;
+constexpr inline double kFermiConstant = 1.1663788e-5;
 
 /// N_A, Avogadro's number in mol^-1 (or if you prefer, electrons/mol when thinking about the matter effect).
-const double kAvogadroConstant = 6.02214076e23;
+constexpr inline double kAvogadroConstant = 6.02214076e23;
 
 /// c, the speed of light (m/s = unitless). Multiply by c in natural units to convert s to m.
-const double kSpeedOfLight = 299792458;
+constexpr inline double kSpeedOfLight = 299792458;
 
 /// hbar, reduced Planck's constant (GeV*s = unitless). Multiply by hbar in natural units to convert s^-1 to GeV.
-const double kReducedPlanckConstant = 6.582119569e-25;
+constexpr inline double kReducedPlanckConstant = 6.582119569e-25;
 
 /// Number of electrons per nucleon in a typical atom. Used for computing the matter effect.
-const double kZPerA = 0.5;
+constexpr inline double kZPerA = 0.5;
 
 /// Unit conversion: m^-1 to eV.
-const double kInversemToeV =
+constexpr inline double kInversemToeV =
     kSpeedOfLight          // 1 m^-1 = SpeedOfLight s^-1.
   * kReducedPlanckConstant // 1 s^-1 = ReducedPlanckConstant GeV.
   * kGeVToeV;              // Finally convert GeV to eV.
 
+// std::pow would be more readable, but it isn't constexpr until C++26.
 /// Given a matter density of electrons, rho in mol/cm^3, multiply by 2*sqrt(2)*FermiConstant and convert units to eV.
-const double kMatterDensityToEffect =
-    sqrt(2) * kFermiConstant * pow(kGeVToeV,-2)
+constexpr inline double kMatterDensityToEffect =
+    M_SQRT2 * kFermiConstant * 1/(kGeVToeV*kGeVToeV)
   * kAvogadroConstant                          // 1 mol = 1 AvogadroConstant.
-  * pow(kInversemToeV*100,3);
+  * (kInversemToeV*100)*(kInversemToeV*100)*(kInversemToeV*100);
 
 }
 }
